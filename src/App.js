fab4e9b65a34c4d4d087399313beb47d01cdde6c
kelvinfotech <kelvinfotech@gmail.com>
2024-05-09 11:04:52 +0300

    React Week Two Code Challenge

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import BotsPage from "./containers/BotsPage"
+import "./App.css"
+
+export default function App() {
+  return (
+    <div className="App">
+      <BotsPage />
+    </div>
+  )
+}
+
+
